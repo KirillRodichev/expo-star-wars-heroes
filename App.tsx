01eabfaaff0f4c5db5e9f1247d08080db299fdb6
Kirill Rodichev <kirill.r@0hub.com>
2025-10-20 16:36:15 +0100

    feat: dummy effect

--- App.tsx
+++ App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Providers } from './src/app/providers';
 import { Navigation } from './src/app/navigation';
 
 export default function App() {
+    useEffect(() => {
+        console.log('dummy effect')
+
+        return () => {
+            console.log('dummy cleanup')
+        }
+    }, []);
+
   return (
     <Providers>
       <Navigation />
